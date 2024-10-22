08774990afd4af04590a824edebf5dfd6094bce5
Angel <apuentest@uniempresarial.edu.co>
2024-10-22 17:49:22 -0500

    Instanciación de clases Cancion y Podcast

---
+++ src/com/angelpuentesdevv/resonancerealm/principal/Principal.java
@@ -0,0 +1,40 @@
+package com.angelpuentesdevv.resonancerealm.principal;
+
+import com.angelpuentesdevv.resonancerealm.modelos.Cancion;
+import com.angelpuentesdevv.resonancerealm.modelos.Podcast;
+import com.sun.tools.javac.Main;
+
+import java.io.PipedOutputStream;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("What Now Remains");
+        miCancion.setAlbum("Islands of the Lost and Forgotten");
+        miCancion.setCantante("HOYO-MIX");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Angel Puentes");
+        miPodcast.setTitulo("Dualismo");
+        miPodcast.setDescripcion("...");
+
+        for (int i = 0; i < 1500; i++) {
+            miCancion.meGusta();
+            miCancion.reproduce();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            miPodcast.meGusta();
+            miPodcast.reproduce();
+        }
+
+        System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de likes: " + miCancion.getTotalDeMeGusta());
+
+        System.out.println("Total de reproducciones: " + miPodcast.getTotalDeReproducciones());
+        System.out.println("Total de likes: " + miPodcast.getTotalDeMeGusta());
+
+    }
+}
