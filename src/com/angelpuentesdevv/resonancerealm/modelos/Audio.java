81236550141adf24244ef58645e7baed6a6c3a8a
Angel <apuentest@uniempresarial.edu.co>
2024-10-22 17:36:48 -0500

    Creación clase audio y subclases

--- src/com/angelpuentesdevv/resonancerealm/modelos/Audio.java
+++ src/com/angelpuentesdevv/resonancerealm/modelos/Audio.java
@@ -2,4 +2,51 @@ package com.angelpuentesdevv.resonancerealm.modelos;
 
 public class Audio {
 
+    private String titulo;
+
+    private int totalDeReproducciones = 0;
+
+    private int totalDeMeGusta = 0;
+
+    private int clasificacion;
+
+    public void meGusta(){
+        this.totalDeMeGusta++;
+    }
+
+    public void reproduce(){
+        this.totalDeReproducciones++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalDeReproducciones() {
+        return totalDeReproducciones;
+    }
+
+    public void setTotalDeReproducciones(int totalDeReproducciones) {
+        this.totalDeReproducciones = totalDeReproducciones;
+    }
+
+    public int getTotalDeMeGusta() {
+        return totalDeMeGusta;
+    }
+
+    public void setTotalDeMeGusta(int totalDeMeGusta) {
+        this.totalDeMeGusta = totalDeMeGusta;
+    }
+
+    public int getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
 }
